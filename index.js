e6517534aaadc8efc7cf6a0a0d97fc9f9deed0dc
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-01 17:39:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var returnObj = Object.assign({}, object)
-  delete returnObj.[key]
+  delete returnObj.key
   return  returnObj
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  //delete object.[key]
+  delete object.key
+  return object
 }
