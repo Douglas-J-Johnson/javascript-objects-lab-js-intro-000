7da21ff8268f989053592ae59d46a443528a8652
Douglas Johnson <douglas.j.johnson@gmail.com>
2019-12-01 17:30:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {key:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  return
+}
+
+function deleteFromObjectByKey(object, key) {
+  return
+}
+
+destructivelyDeleteFromObjectByKey(object, key) {
+  return
+}
